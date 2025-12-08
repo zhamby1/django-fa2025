5e17171f1b1c0c6427b971dc0ffa8034949845ad
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-12-08 15:11:46 -0500

    added view

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     #we can list all of our urls for our website here..we will first start with the index or homepage
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/remove', views.post_remove, name='post_remove'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import redirect
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -10,3 +13,58 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #when rendering a webpage we return a render of the request, the html page we want to load, and any data we want to pass to that webpage (usually in the form of a db query)
     return render(request, 'blog/post_list.html', { 'posts' : posts})
+
+#see one post details
+def post_detail(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form':form})
+
+@login_required 
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form':form})
+    
+
+#create our list of drafts by checking if the have a publish date or not
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts' : posts})
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+    return redirect('post_list')
